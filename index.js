bedb372e6c080ef2aa90febf4ca1291a6d1747ad
AshutoshSingh00001 <ashutoshsingh10@icloud.com>
2024-03-18 00:43:21 +0530

    tweer

--- index.js
+++ index.js
@@ -241,12 +241,12 @@ const matchingVotesAye = aye.filter(vote => {
             console.log(postId)
             console.log(identities.join(', '))
             console.log(twitter)
-            const post = async() => {
-              if(effectiveVotes > 500) {
-                return tweet();
-              }
-            }
-            return post()
+            // const post = async() => {
+            //   if(effectiveVotes > 500) {
+            //     return tweet();
+            //   }
+            // }
+            return tweet()
         } else {
             console.log(`${voteType} failed`);
         }
