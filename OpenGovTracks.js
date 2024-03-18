70a1d186332c66bc2dc60f12cf68cd4ea9cfb0fb
AshutoshSingh00001 <ashutoshsingh10@icloud.com>
2024-03-18 15:15:13 +0530

    referendum participants

--- OpenGovTracks.js
+++ OpenGovTracks.js
@@ -47,11 +47,12 @@ const OpenGovTracks = async() => {
         const referendum_locked = Math.floor(responseData.data.referendum_locked / 1000000000000)
         const formattedreferendum_locked = referendum_locked.toLocaleString();
         const referendum_lockedusd = (referendum_locked * dotPrice).toLocaleString()
-        const referendum_participate = (responseData.data.referendum_participate / 1000000000000).toLocaleString()
+        const formattedReferendumParticipates = Math.floor(responseData.data.referendum_participate / 1000000000000)
+        const referendum_participate = formattedReferendumParticipates.toLocaleString()
         const voting_total = responseData.data.voting_total
         const confirm_total = responseData.data.confirm_total
-        const next_burn = (referendum_locked / 1 * 100).toLocaleString()
-        const next_burn_usd = ((referendum_locked / 1 * 100) * dotPrice).toLocaleString()
+        const next_burn = (referendum_locked / 100).toLocaleString()
+        const next_burn_usd = ((referendum_locked / 100) * dotPrice).toLocaleString()
         console.log(next_burn, referendum_locked,referendum_participate)
         const tweetData = `OpenGov Treasury Overview \n Available ${formattedreferendum_locked} DOT ($${referendum_lockedusd} USD) \n Next Burn ${next_burn} DOT ($${next_burn_usd} USD) \n DOT Price $${dotPrice} USD \n Active Voters ${referendum_participate} \n Active Referendums ${voting_total} \n Confirming Referendums ${confirm_total} \n\n #DOT #POLKADOT #OpenGOV #votes  `
         const tweet = async () => {
