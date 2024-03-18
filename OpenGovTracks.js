45cb685c144c950aa26b15f8540057d00bb61248
AshutoshSingh00001 <ashutoshsingh10@icloud.com>
2024-03-18 15:32:31 +0530

    fix in decimals

--- OpenGovTracks.js
+++ OpenGovTracks.js
@@ -44,10 +44,10 @@ const OpenGovTracks = async() => {
             console.log(dotPrice)
     
         const responseData = await response.json();
-        const referendum_locked = Math.floor(responseData.data.referendum_locked / 1000000000000)
+        const referendum_locked = Math.floor(responseData.data.referendum_locked / 10000000000)
         const formattedreferendum_locked = referendum_locked.toLocaleString();
         const referendum_lockedusd = (referendum_locked * dotPrice).toLocaleString()
-        const formattedReferendumParticipates = Math.floor(responseData.data.referendum_participate / 1000000000000)
+        const formattedReferendumParticipates = Math.floor(responseData.data.referendum_participate / 10000000000)
         const referendum_participate = formattedReferendumParticipates.toLocaleString()
         const voting_total = responseData.data.voting_total
         const confirm_total = responseData.data.confirm_total
