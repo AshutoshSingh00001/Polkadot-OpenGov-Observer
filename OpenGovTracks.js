79a372ba1445e12f1a987d46aa24764fa17f8621
AshutoshSingh00001 <ashutoshsingh10@icloud.com>
2024-03-22 10:57:18 +0530

    fix

--- OpenGovTracks.js
+++ OpenGovTracks.js
@@ -3,7 +3,7 @@ const { twitterClient } = require("./twitterClient.js")
 const CronJob = require("cron").CronJob;
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => {
